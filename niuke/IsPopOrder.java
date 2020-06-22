c5f08fb0ffbaea67c5484cf3ee0d49c3e1ecba24
Zhounning <1767508581@qq.com>
2020-06-22 23:57:55 +0800

    2020-06-22提交

---
+++ niuke/IsPopOrder.java
@@ -0,0 +1,42 @@
+package niuke;
+
+
+import java.util.Stack;
+
+public class IsPopOrder {
+    public boolean isPopOrder(int [] pushA,int [] popA) {
+        if (pushA==null||popA==null) {
+            return false;
+        }
+
+        if (pushA.length!=popA.length) {
+            return false;
+        }
+        boolean res = true;
+        int i = 0;
+        int j = 0;
+        Stack<Integer> myStack = new Stack<>();
+        // myStack.push(pushA[0]);
+
+        while (true) {
+            
+            while(!myStack.isEmpty()&&i<popA.length&&myStack.peek()==popA[i]) {
+                myStack.pop();
+                i++;
+            }
+
+            if (j<pushA.length) {
+                myStack.push(pushA[j++]);
+            }else{
+                if (myStack.isEmpty()&&i==popA.length) {
+                    res=true;
+                }else{
+                    res = false;
+                }
+                break;
+            }
+        }
+
+        return res;
+    }
+}
---
+++ niuke/minStack.java
@@ -0,0 +1,47 @@
+package niuke;
+
+import java.util.Stack;
+
+public class minStack {
+    private Stack<Integer> mystack = new Stack<>();
+    private Stack<Integer> minstack = new Stack<>();
+
+    public void push(int node) {
+        mystack.push(node);
+        //判断最小值的push
+        if (minstack.isEmpty()) {
+            minstack.push(node);
+        } else {
+            if (node<=minstack.peek()) {
+                minstack.push(node);
+            }
+        }
+
+    }
+    
+    public void pop() {
+        if (!mystack.isEmpty()) {
+            int res = mystack.pop();
+            if (res==minstack.peek()) {
+                minstack.pop();
+            }
+        }
+    }
+    
+    public int top() {
+        //这里如果为空很不好处理，不知道乍整
+       try {
+           int res =  mystack.peek();
+           return res;
+       } catch (Exception e) {
+           //TODO: handle exception
+            e.printStackTrace();
+        }
+
+        return mystack.peek();
+    }
+    
+    public int min() {
+        return minstack.peek();
+    }
+}
