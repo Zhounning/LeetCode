ad6b77d0b9946da4488fce96b5a22dfc0f7c40e3
Zhounning <1767508581@qq.com>
2020-06-23 22:59:43 +0800

    2020-06-23提交

---
+++ niuke/MoreThanHalfNum_Solution.java
@@ -0,0 +1,36 @@
+package niuke;
+
+public class MoreThanHalfNum_Solution {
+
+    public int moreThanHalfNum_Solution(int [] array) {
+        if (array==null||array.length==0) {
+            return 0;
+        }
+        int res = array[0];
+        int count = 0;
+        for (int i : array) {
+            if (res==i) {
+                ++count;
+
+            }else{
+                if (count==0) {
+                    res = i;
+                    count=1;
+                }else{
+                    --count;
+                }
+            }
+        }
+        count=0;
+        for (int j : array) {
+            if (j==res) {
+                ++count;   
+            }
+        }
+
+        if (count<array.length/2) {
+            return 0;
+        }
+        return res;
+    }
+}
