c11002b92b74834e23e05590af51a53df75bb4e1
Zhounning <1767508581@qq.com>
2020-06-24 23:23:18 +0800

    2020-02-24打卡完成

---
+++ niuke/FindGreatestSumOfSubArray.java
@@ -0,0 +1,19 @@
+package niuke;
+
+/**
+ * FindGreatestSumOfSubArray
+ */
+public class FindGreatestSumOfSubArray {
+
+    public int findGreatestSumOfSubArray(int[] array) {
+   
+        int dp = array[0];
+        int res = dp;
+        for (int i = 1; i < array.length; i++) {
+           dp = Math.max(array[i], array[i]+dp);
+           res = Math.max(dp, res);
+        }
+        return res;
+
+    }
+}
---
+++ niuke/NumberOf1Between1AndN_Solution.java
@@ -0,0 +1,16 @@
+package niuke;
+
+public class NumberOf1Between1AndN_Solution {
+    public int numberOf1Between1AndN_Solution(int n) {
+        // 链接：https://www.nowcoder.com/questionTerminal/bd7f978302044eee894445e244c7eee6?f=discussion
+        // 来源：牛客网
+        
+        if(n <= 0)return 0;
+        int count = 0;
+        for(long i = 1; i <= n; i *= 10){
+            long diviver = i * 10;          
+            count += (n / diviver) * i + Math.min(Math.max(n % diviver - i + 1, 0), i);
+        }
+        return count;
+    }
+}
