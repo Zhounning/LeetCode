cadfc1abf2d0ac88172bcae967ddf0957834244a
Zhounning <1767508581@qq.com>
2020-07-24 20:15:15 +0800

    7-24

--- leetCode/maxDepth.java
+++ leetCode/maxDepth.java
@@ -24,5 +24,14 @@ public class maxDepth {
 
 }
 
-
+/**
+ * Definition for a binary tree node.
+ * 
+ **/
+class TreeNode {
+      int val;
+      TreeNode left;
+       TreeNode right;
+      TreeNode(int x) { val = x; }
+  }
  
